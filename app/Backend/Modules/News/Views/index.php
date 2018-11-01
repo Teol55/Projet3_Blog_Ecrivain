d26273aa601a694a711eea7801132c40a62ed600
C_MARTIN <m.ch55000@gmail.com>
2018-11-01 09:20:58 +0100

    modification de la vue index admin

--- app/Backend/Modules/News/Views/index.php
+++ app/Backend/Modules/News/Views/index.php
@@ -1,20 +1,30 @@
 <div class="container">
-    <p style="text-align: center">Il y a actuellement
-        <?= $nombreNews ?> news. En voici la liste :</p>
+    <div class="panel panel-primary">
 
-    <table>
-        <tr>
-            <th>Auteur</th>
-            <th>Titre</th>
-            <th>Date d'ajout</th>
-            <th>Dernière modification</th>
-            <th>Action</th>
-        </tr>
-        <?php
+        <table class="table table-striped table-condensed">
+            <div class="panel-heading">
+                <h3 class="panel-title">Il y a actuellement
+                    <?= $nombreNews ?> Chapitre. En voici la liste :</h3>
+            </div>
+            <thead>
+                <tr>
+                    <th>Chapitre</th>
+                    <th>Titre</th>
+                    <th>Date d'ajout</th>
+                    <th>Dernière modification</th>
+                    <th>Action</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
 foreach ($listeNews as $news)
 {
-  echo '<tr><td>', $news['chapite'], '</td><td>', $news['titre'], '</td><td>le ', $news['dateAjout']->format('d/m/Y à H\hi'), '</td><td>', ($news['dateAjout'] == $news['dateModif'] ? '-' : 'le '.$news['dateModif']->format('d/m/Y à H\hi')), '</td><td><a href="news-update-', $news['id'], '.html"><img src="/images/update.png" alt="Modifier" /></a> <a href="news-delete-', $news['id'], '.html"><img src="/images/delete.png" alt="Supprimer" /></a></td></tr>', "\n";
+  echo '<tr><td>', $news['chapitre'], '</td><td>', $news['titre'], '</td><td>le ', $news['dateAjout']->format('d/m/Y à H\hi'), '</td><td>', ($news['dateAjout'] == $news['dateModif'] ? '-' : 'le '.$news['dateModif']->format('d/m/Y à H\hi')), '</td><td><a href="news-update-', $news['id'], '.html"><i class="fa fa-pencil"></i></a><a href="news-delete-', $news['id'], '.html"><i class="fa fa-trash"></i></a></td></tr>', "\n";
 }
 ?>
-    </table>
+
+            </tbody>
+        </table>
+    </div>
 </div>
